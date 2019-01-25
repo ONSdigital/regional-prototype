d516c36e81d86f5e2815da85c223ced09ec1c555
Michael Norton <michaelnorton@live.co.uk>
2019-01-25 11:15:40 +0100

    Updates

---
+++ precache-manifest.f7f8e4a9936b17962381bbe84fca848b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d354507e24c43f9553c0",
+    "url": "/regional-prototype/static/js/runtime~main.d354507e.js"
+  },
+  {
+    "revision": "f1b4340f3cd87c9cc7d5",
+    "url": "/regional-prototype/static/js/main.f1b4340f.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/regional-prototype/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "f1b4340f3cd87c9cc7d5",
+    "url": "/regional-prototype/static/css/main.369d0517.chunk.css"
+  },
+  {
+    "revision": "3aa2cb2a7478d1114ea3ef06a2ebd584",
+    "url": "/regional-prototype/index.html"
+  }
+];
