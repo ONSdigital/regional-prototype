61b6d4a61024fadf9dace246a09dcee5f17b2fc0
robchamberspfc <robchamberspfc@gmail.com>
2019-04-17 18:18:07 +0100

    fix warning alignment

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Warning = () => (
 		<div className="beta-banner">
-			<div className="wrapper">
+			<div className="container">
 				<p className= "margin-top--0 margin-bottom--0"><span className="font-size--16"><strong className="warning">Warning</strong> this is a prototype and the content may not be complete or accurate</span></p>
 			</div>
 		</div>
