f8339024e6b6af1319e991a6327b18eb9db0bf23
robchamberspfc <robchamberspfc@gmail.com>
2019-04-17 17:28:10 +0100

    add warning banner

---
+++ src/components/Warning.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Warning = () => (
+		<div className="beta-banner">
+			<div className="wrapper">
+				<p className= "margin-top--0 margin-bottom--0"><span className="font-size--16"><strong className="warning">Warning</strong> this is a prototype and the content may not be complete or accurate</span></p>
+			</div>
+		</div>
+)
+
+export default Warning
